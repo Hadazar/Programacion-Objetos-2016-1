909965c7b816b8ca400bcb63664c8bf8d1509d2e
Hadazar <hadazar@unal.edu.co>
2016-02-26 11:32:20 -0500

    Se agrega funcion dibujar avion

--- Avion/src/avion/Avion.java
+++ Avion/src/avion/Avion.java
@@ -18,7 +18,7 @@ public class Avion {
         
         java.util.Scanner Leer = new java.util.Scanner(System.in);
         
-        boolean[][] sillasEjecutivas = new boolean[2][8];
+        boolean[][] sillasEjecutivas = new boolean[2][4];
         boolean[][] sillasEconomicas = new boolean[7][6];
         String[] nombres = new String[50];
         int[] cedulas = new int[50];
@@ -27,9 +27,38 @@ public class Avion {
         String clase = Leer.next();
         String ubicacion = Leer.next();
         
+        dibujarAvion(sillasEjecutivas, sillasEconomicas);
+        
     }
     
     //Funciones:
+    
+    public static void dibujarAvion(boolean[][] sillasEjecutivas, boolean[][] sillasEconomicas){
+        
+        for(int i = 0; i < 2; i++){
+            for(int j = 0; j < 4; j++){
+                if(sillasEjecutivas[i][j] == true){
+                    System.out.print("X");
+                }else{System.out.print("▄");
+                if(j == 1){
+                System.out.print("    ");
+                }else{System.out.print("  ");}
+            }
+            System.out.println("");
+            }
+        }
+        for(int i = 0; i < 7; i++){
+            for(int j = 0; j < 6; j++){
+                if(sillasEconomicas[i][j] == true){
+                    System.out.print("X");
+                }else{System.out.print("▓");}
+                if(j == 2){
+                System.out.print("    ");
+                }else{System.out.print("  ");}
+            }
+            System.out.println("");
+        }
+    }
     
     public static boolean[][] asignacionDeSillas(boolean[][] sillas, String clase, String ubicacion){
         
